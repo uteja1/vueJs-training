13979ebe6e09ef8132b394d3523ada34dee48195
Uday Teja k <uteja70@gmail.com>
2022-10-11 12:22:50 +0530

    registering a sample component in another component

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,50 +1,20 @@
 <template>
-
-  <button class = "btn btn-primary" @click="increment">in {{counter}}</button>
-  <p :class="counter % 2 === 0 ? 'blue-text' : 'red-text'">finalPrice : {{finalPrice}}</p>
-
+  <h3>Learning Vue 3</h3>
+  <Sample />
 </template>
 
-<script lang="ts">  
+<script lang="ts">
 import { defineComponent } from 'vue';
+import Sample from './components/Sample.vue'
 
 export default defineComponent({
   name: 'App',
-  data: function () {
-    return {
-      productCostBreakdown: {
-        basePrice: 100,
-        taxRate: 10,
-        discountRate: 5
-      },
-      counter:0
-    } 
-  },
-  computed: {
-    finalPrice: function () {
-      return this.productCostBreakdown.basePrice
-        + (this.productCostBreakdown.basePrice * this.productCostBreakdown.taxRate) / 100
-        + (this.productCostBreakdown.basePrice * this.productCostBreakdown.discountRate) / 100;
-    }
-  },
-  methods: {
-    increment() {
-      ++this.counter;
-    }
+  components: {
+    Sample
   }
 });
 </script>
 
 <style scoped>
-.red-text {
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    color: red;
-    font-size: 5rem;
-  }
 
-  .blue-text {
-      font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-      color: blue;
-      font-size: 5rem;
-    }
 </style>
---
+++ vue-app/src/components/Sample.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3>Sample Component</h3>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Sample'
+});
+</script>
